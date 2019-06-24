1535a14ccb2de86ab25da391b8b6b558194ec79e
dustyfingers <dustyfingers01257@gmail.com>
2019-06-23 22:55:48 -0600

    learned about the restrictions of jsx

--- sec-3/react-complete-guide/src/App.js
+++ sec-3/react-complete-guide/src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a react app!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a react app!'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app!</h1>
+      </div>
+    );
   }
 }
 
