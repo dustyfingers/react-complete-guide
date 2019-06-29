81a58c8b7049d90ccfa7c36147afb06b8ad4d69c
dustyfingers <dustyfingers01257@gmail.com>
2019-06-29 09:13:26 -0600

    learned about how to use methods on class based components

--- sec-3/react-complete-guide/src/App.js
+++ sec-3/react-complete-guide/src/App.js
@@ -11,12 +11,25 @@ class App extends Component {
       { name: 'Jim', age: 42 }
     ]
   }
+
+  switchNameHandler = () => {
+    // console.log('Switch name button was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Maxamilian', age: 86 },
+        { name: 'Zenu', age: 17 },
+        { name: 'Alan', age: 34 },
+        { name: 'Jim', age: 25 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
