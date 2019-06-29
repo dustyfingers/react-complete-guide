108ed6c066e6a8f62e82424fcc9247f02f7791f6
dustyfingers <dustyfingers01257@gmail.com>
2019-06-29 08:57:38 -0600

    learning bout props and state

--- sec-3/react-complete-guide/src/App.js
+++ sec-3/react-complete-guide/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 86 },
+      { name: 'Zenu', age: 17 },
+      { name: 'Alan', age: 34 },
+      { name: 'Jim', age: 42 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} >I am a peron with props.children! </Person>
       </div>
     );
   }
