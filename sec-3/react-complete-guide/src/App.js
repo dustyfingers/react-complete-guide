a788839209c4518093f5fc2935f9baad542ee5c4
dustyfingers <dustyfingers01257@gmail.com>
2019-06-29 19:19:58 -0600

    learned about using inline styles and stylsheets with react

--- sec-3/react-complete-guide/src/App.js
+++ sec-3/react-complete-guide/src/App.js
@@ -38,11 +38,22 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App" >
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('LOUIE!!!')} >Switch Name</button>
+        <button
+          onClick={() => this.switchNameHandler('LOUIE!!!')}
+          style={style}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
